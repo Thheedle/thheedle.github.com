57903eb0ee2fd98ae66403ce72099d251c5c2c56
seannmmcculloch <seannmmcculloch@gmail.com>
2018-09-27 14:18:00 -0700

    fixed home link and added activeHlink to main subpages

--- scripts/js/hlinkScript.js
+++ scripts/js/hlinkScript.js
@@ -3,12 +3,26 @@ var hlinks = document.getElementsByClassName("hlink");
 for (var i = 0; i < hlinks.length; i++) {
 	hlinks[i].addEventListener("mouseover", overButton);
 	hlinks[i].addEventListener("mouseout", offButton);
+	if( hlinks[i].classList.contains("activeHlink")) {
+		hlinks[i].style.textShadow = "0 0 10px #888888, 0 0 10px #AAAAAA";
+	}
 }
 
 function overButton() {
-	this.style.textShadow = "0 0 10px #888888, 0 0 10px #AAAAAA";
+	if( this.classList.contains("activeHlink")) {
+		this.style.textShadow = "0 0 2px #AAAAAA";
+
+	} else {
+		this.style.textShadow = "0 0 10px #888888, 0 0 10px #AAAAAA";
+	}
 }
 
 function offButton() {
-	this.style.textShadow = "0 0 2px #AAAAAA";
+	if( this.classList.contains("activeHlink")) {
+		this.style.textShadow = "0 0 10px #888888, 0 0 10px #AAAAAA";
+	} else {
+		this.style.textShadow = "0 0 2px #AAAAAA";
+	}
+	
+	
 }
