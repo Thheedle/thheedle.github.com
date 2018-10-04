dd48058c77eb07076ae0f892323a464818a2c9a0
seannmmcculloch <seannmmcculloch@gmail.com>
2018-10-04 00:57:32 -0700

    changed the homepage to bootstrap, working on employees

---
+++ scripts/pages/index2.js
@@ -0,0 +1,11 @@
+/*
+This is where page-specific JAVASCRIPT will go
+
+Copy and rename this file to match the html and js files
+
+For example:
+
+example.html
+example.css
+example.js       <--- this one
+*/
