f6a5de0dd5dc1ae0fccd8542b718afb139a607cd
seannmmcculloch <seannmmcculloch@gmail.com>
2018-10-07 21:25:47 -0700

    fix syntax error in themescript

--- scripts/js/themeScript.js
+++ scripts/js/themeScript.js
@@ -1,9 +1,10 @@
 checkRedirectToMobile() {
 	if (screen.width <= 699) {
-	let loc = window.location;
-	let newURL = loc.protocol + "//m." + loc.host + loc.pathname;
-	window.location.href = newURL
-	console.log(window.location);
+		let loc = window.location;
+		let newURL = loc.protocol + "//m." + loc.host + loc.pathname;
+		window.location.href = newURL
+		console.log(window.location);
+	}
 }
 
 
