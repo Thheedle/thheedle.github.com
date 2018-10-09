83d9983d50c9277b57b286e504d8060b8c8ceb0b
seannmmcculloch <seannmmcculloch@gmail.com>
2018-10-08 21:17:49 -0700

    added schhedule files and node.js

---
+++ scripts/schhedule/schemas.js
@@ -0,0 +1,14 @@
+exports.class = () => (
+  {
+    CRN: Number,
+    Instructor: String,
+    Location: Array,
+    Title: String,
+    Time: Array,
+    GUR: String,
+    Credits: Number,
+    Cost: Number,
+    StartDate: Date,
+    EndDate: Date,
+  }
+);
